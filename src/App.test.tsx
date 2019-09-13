b3bc0c9c8c98393a030cf9d9aded84f6c03398c6
Ali Raza Alvi <aliraza432@gmail.com>
2019-09-13 09:49:24 +0200

    Added the test for app component.

---
+++ src/App.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { renderWithRedux } from './testing/testingHelper';
+import App from './App';
+
+afterEach(cleanup);
+
+const label = 'Boilerplate';
+test('can render default view', async () => {
+    const { getByText } = renderWithRedux(<App />);
+
+    const elem = getByText(label);
+    expect(elem.innerHTML).toBe(label);
+});
